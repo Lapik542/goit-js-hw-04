f7a26a91127655034cc590c83df0c8577773409c
Гавриш Олександр <132341505+Lapik542@users.noreply.github.com>
2023-11-07 14:50:02 +0200

    main

---
+++ script-1.js
@@ -0,0 +1,42 @@
+const cars = [
+   {
+   modal: 'Audio',
+   price: 10000,
+   speed: '240km/gh',
+   currency: 'USD',
+   
+   engine: {
+      modal: 'V2',
+      price: 2000,
+   },
+   },
+   {
+   modal: 'BMV',
+   price: 18000,
+   speed: '340km/gh',
+   currency: 'USD',
+   
+   engine: {
+      modal: 'V6',
+      price: 6000,
+   },
+   },
+   {
+   modal: 'Tesla',
+   price: 20000,
+   speed: '400km/gh',
+   currency: 'USD',
+   
+   engine: {
+      modal: 'V12',
+      price: 14000,
+   },
+   },
+];
+cars[1].currency = 'UAH';
+cars[0].price = 12000;
+console.log(cars[0]);
+console.log(cars[1]);
+
+
+
