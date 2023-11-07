b29aa93f120f70b6a07d092ab4b5fcef62751f81
Гавриш Олександр <132341505+Lapik542@users.noreply.github.com>
2023-11-07 21:32:51 +0200

    Update script-1.js

--- script-1.js
+++ script-1.js
@@ -1,52 +1,28 @@
-const cars = [
-   {
-   modal: 'Audio',
-   price: 10000,
-   speed: '240km/gh',
-   currency: 'USD',
-   
-   engine: {
-      modal: 'V2',
-      price: 2000,
-   },
-   },
-   {
-   modal: 'BMV',
-   price: 18000,
-   speed: '340km/gh',
-   currency: 'USD',
-   
-   engine: {
-      modal: 'V6',
-      price: 6000,
-   },
-   },
-   {
-   modal: 'Tesla',
-   price: 20000,
-   speed: '400km/gh',
-   currency: 'USD',
-   
-   engine: {
-      modal: 'V12',
-      price: 14000,
-   },
-   },
-];
-cars[1].currency = 'UAH';
-cars[0].price = 12000;
-console.log(cars[0]);
-console.log(cars[1]);
-console.log(cars[2].modal
-   + ' ' +
-   cars[2].engine.modal
-   + ', ' +
-   cars[2].speed
-   + ', ' +
-   cars[2].price
-   + ' ' +
-   cars[2].currency
-);
+function isEnoughCapacity(products, containerSize) {
+   let sum = 0;
+   for (const key in products) {
+      const value = products[key];
+      sum += value
+   }
+   if (sum <= containerSize) {
+      return true;
+   } else {
+      return false;
+   }
+}
 
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
 
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
 
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)
+); // false
