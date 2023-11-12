7a2b12696cca6d01ddd893bb2d56d042f9da9671
Гавриш Олександр <132341505+Lapik542@users.noreply.github.com>
2023-11-12 21:48:50 +0200

    main

--- script-3.js
+++ script-3.js
@@ -1,10 +1,10 @@
 const profile = {
-  username: "Jacob",
-playTime: 300,
+   username: "Jacob",
+   playTime: 300,
 
 changeUsername(newName) {
   this.username = newName;
-},
+   },
 
 updatePlayTime(hours) {
   this.playTime += hours;
